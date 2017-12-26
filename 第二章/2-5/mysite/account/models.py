cfef7cd849ce394915712b803f73b24dc9ea88c6
xwp <xwp_fullstack@163.com>
2017-12-26 16:59:22 +0800

    下一步，编辑个人信息

---
+++ 第二章/2-5/mysite/account/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,unique=True)
+    birth = models.DateField(blank=True,null=True)
+    phone = models.CharField(max_length=20,null=True)
